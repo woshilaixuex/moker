e8275fe2fc78a77439046891bba79d829600ac03
linyugang <2904056707@qq.com>
2024-05-04 15:53:28 +0800

    test(usercenter):first commit

---
+++ app/usercenter/internal/svc/servicecontext.go
@@ -0,0 +1,15 @@
+package svc
+
+import (
+	"github.com/moker/app/usercenter/internal/config"
+)
+
+type ServiceContext struct {
+	Config config.Config
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	return &ServiceContext{
+		Config: c,
+	}
+}
